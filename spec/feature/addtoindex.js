8cc9608e1ae114de0d11974d4802da4388105dec
Charly Mannion <charlottemannion100@gmail.com>
2018-05-24 14:26:58 +0100

    return home buttons added to add and view listings views

---
+++ spec/feature/addtoindex.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'return to index from add page' : function (client) {
+    client
+      .url("http://localhost:3000/")
+      .waitForElementVisible('body', 2000)
+      .assert.containsText('body', 'Hello, MakersBnB!')
+      .waitForElementVisible('#addListings', 5000)
+      .click('#addListings')
+      .pause(1000)
+      .assert.containsText('body', 'Add a listing')
+      .click('#backHome')
+      .pause(1000)
+      .assert.containsText('body', 'Hello, MakersBnB!')
+      .end()
+  }
+}
